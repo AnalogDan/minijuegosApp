0d9120975eb406a025fdff2e7f24b23652daf82c
Emmanuel Ganu <robot1emm@gmail.com>
2026-05-16 12:38:06 -0600

    BrickGame: Botón de menú funciona, añadí la clase y el view principal del juego, se modificó el AndroidManifest

--- app/src/main/java/com/example/minijuegosapp/MainActivity.java
+++ app/src/main/java/com/example/minijuegosapp/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
 import android.widget.Button;
 
+import com.example.minijuegosapp.brickgame.Brickgame;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -29,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //BrickGame
+        Button btn_brickgame = findViewById(R.id.brickgameButton);
+        btn_brickgame.setOnClickListener(v ->{
+            Intent intent = new Intent(MainActivity.this, Brickgame.class);
+            startActivity(intent);
         });
     }
 }
