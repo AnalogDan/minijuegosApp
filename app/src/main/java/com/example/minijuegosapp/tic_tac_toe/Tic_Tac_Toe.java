a4f2adfb0f79107bfc0aa6511be557883a3c992e
Imanol <imanolraamirez@gmail.com>
2026-05-18 09:49:50 -0600

    ttt:se añadio en el manifest

---
+++ app/src/main/java/com/example/minijuegosapp/tic_tac_toe/Tic_Tac_Toe.java
@@ -0,0 +1,26 @@
+package com.example.minijuegosapp.tic_tac_toe;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.minijuegosapp.R;
+
+public class Tic_Tac_Toe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tic_tac_toe);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
