1cfb2162bb4e2a06a7b957590615b1b1fa542923
Emmanuel Ganu <robot1emm@gmail.com>
2026-05-19 19:46:25 -0600

    Global: Nuevo menú de inicio con animaciones e imagenes

--- app/src/main/java/com/example/minijuegosapp/MainActivity.java
+++ app/src/main/java/com/example/minijuegosapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.minijuegosapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.minijuegosapp.tic_tac_toe.AddPlayers;
+import com.google.android.material.card.MaterialCardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
+import android.view.MotionEvent;
 import android.widget.Button;
 
 import com.example.minijuegosapp.brickgame.Brickgame;
@@ -20,24 +25,49 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        //Snake button
-        Button btn = findViewById(R.id.snakeButton);
-        btn.setOnClickListener(v -> {
+        //Obtener las tarjetas/botones a utilizar
+        MaterialCardView cardSnake = findViewById(R.id.cardSnake);
+        MaterialCardView cardBrickGame = findViewById(R.id.cardBrickGame);
+        MaterialCardView cardTicTacToe = findViewById(R.id.cardTicTacToe);
+
+        setupCardAnimation(cardSnake);
+        setupCardAnimation(cardBrickGame);
+        setupCardAnimation(cardTicTacToe);
+
+        cardSnake.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, Snake.class);
             startActivity(intent);
         });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        //BrickGame
-        Button btn_brickgame = findViewById(R.id.brickgameButton);
-        btn_brickgame.setOnClickListener(v ->{
+        cardBrickGame.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, Brickgame.class);
             startActivity(intent);
+        });
+
+        cardTicTacToe.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddPlayers.class);
+            startActivity(intent);
+        });
+    }
+    //Animaciones de las tarjetas/botones
+    @SuppressLint("ClickableViewAccessibility")
+    private void setupCardAnimation(MaterialCardView card) {
+
+        card.setOnTouchListener((v, event) -> {
+
+            switch (event.getAction()) {
+
+                case MotionEvent.ACTION_DOWN:
+                    v.animate().scaleX(0.96f).scaleY(0.96f).setDuration(100).start();
+                    break;
+
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    v.animate().scaleX(1f).scaleY(1f).setDuration(100).start();
+                    break;
+            }
+
+            return false;
         });
     }
 }
